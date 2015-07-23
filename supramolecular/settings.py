92dc0b21b22d4043929038ae09ee4e6ea3f96d50
Varvara Efremova <varvara@echus.co>
2015-07-23 05:39:28 +0000

    Added CORS module

--- supramolecular/settings.py
+++ supramolecular/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'bindsim',
     'bindsim_client',
     'bindfit',
@@ -45,6 +46,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +123,6 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
 }
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = True
