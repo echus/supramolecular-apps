75f12fac4a2bfd141cf1167b2f2e410977b5a89e
Varvara Efremova <varvara@echus.co>
2015-07-27 10:14:28 +0000

    Added basic logging setup

--- supramolecular/settings.py
+++ supramolecular/settings.py
@@ -126,3 +126,37 @@ REST_FRAMEWORK = {
 
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'supramolecular.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'supramolecular': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
