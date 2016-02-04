5af2cb2539ff0b9fdf728265079ddb5454bcdd49
Varvara Efremova <varvara@echus.co>
2016-02-04 08:03:35 +0000

    Working de Levie uncertainty calculation

--- bindfit/fitter.py
+++ bindfit/fitter.py
@@ -4,12 +4,14 @@ from __future__ import print_function
 from math import sqrt
 from copy import deepcopy
 import time
+from itertools import product
+
 import numpy as np
 import numpy.matlib as ml
-import lmfit
 
 import scipy
 import scipy.optimize
+from scipy import stats
 
 from . import functions
 from . import helpers 
@@ -95,116 +97,96 @@ class Fitter():
         # Save time taken to fit
         self.time = toc - tic 
 
+        # Save raw optimised params arra
+        self._params_raw = result.x
+
+        # Postprocess (denormalise) and save fitted data
+        fit = self._postprocess(self.ydata, fit_norm)
+        self.fit = fit
+
+        self.residuals = residuals
+
+        self.coeffs = coeffs
+
+        # Calculate host molefraction from complexes and add as first row
+        molefrac_host = np.ones(molefrac.shape[1])
+        molefrac_host -= molefrac.sum(axis=0)
+        self.molefrac = np.vstack((molefrac_host, molefrac))
+
+        # Calculate fit uncertainty statistics
+        ci = self.statistics()
+
         # Save final optimised parameters and errors as dictionary
-        self.params = { key: {"value": param, "stderr": None, "init": params_init[key]} 
-                        for (key, param) 
-                        in zip(sorted(params_init), result.x) }
+        self.params = { name: {"value": param, "stderr": stderr, "init": params_init[name]} 
+                        for (name, param, stderr) 
+                        in zip(sorted(params_init), result.x, ci) }
 
         logger.debug("Fitter.run: PARAMS DICT")
         logger.debug(self.params)
 
-        # Postprocess (denormalise) and save fitted data
-        fit = self._postprocess(self.ydata, fit_norm)
-        self.fit = fit
-
-        self.residuals = residuals
-
-        self.coeffs = coeffs
-
-        # Calculate host molefraction from complexes and add as first row
-        molefrac_host = np.ones(molefrac.shape[1])
-        molefrac_host -= molefrac.sum(axis=0)
-        self.molefrac = np.vstack((molefrac_host, molefrac))
-
-    def run(self, params_init):
-        """
-        Arguments:
-            params: dict  Initial parameter guesses for fitter    
-        """
-        logger.debug("Fitter.fit: called. Input params:")
-        logger.debug(params_init)
-        
-        p = lmfit.Parameters()
-        for key, value in params_init.items():
-            p.add(key, value=value)
-        
-        # Run optimizer 
-        x = self.xdata
-        y = self._preprocess(self.ydata)
-
-        tic = time.clock()
-        mini = lmfit.Minimizer(self.function.objective, p, fcn_args=(x, y))
-        result = mini.minimize(method="lbfgsb", tol=1e-18)
-        #ci = lmfit.conf_interval(mini, result)
-        #logger.debug(str(lmfit.printfuncs.report_ci(ci)))
-        #result = lmfit.minimize(self.function.objective, p, args=(x, y), method="nelder", tol=1e-18)
-        #scipy.optimize.minimize(self.function.objective, p0, args=(x, y), method="L-BFGS-B", tol=1e-18)
-        toc = time.clock()
-
-        logger.debug("Fitter.run: FIT FINISHED")
-        logger.debug(lmfit.fit_report(result.params))
-        logger.debug("Fitter.run: Fitter.function")
-        logger.debug(self.function)
-        if hasattr(result, "success"):
-            logger.debug("result.success")
-            logger.debug(result.success)
-        if hasattr(result, "message"):
-            logger.debug("result.message")
-            logger.debug(result.message)
-        logger.debug("result.nfev")
-        logger.debug(result.nfev)
-        logger.debug("result.init_vals")
-        logger.debug(result.init_vals)
-
-        # Calculate fitted data with optimised parameters
-        fit_norm, residuals, coeffs, molefrac = self.function.objective(
-                                                    result.params, 
-                                                    x, 
-                                                    y, 
-                                                    detailed=True)
-
-        # Save time taken to fit
-        self.time = toc - tic 
-
-        # Save final optimised parameters and errors as dictionary
-        #self.params = result.params.valuesdict()
-        self.params = { key: {"value": param.value, "stderr": param.stderr, "init": params_init[key]} for (key, param) in result.params.items() }
-
-        logger.debug("Fitter.run: PARAMS")
-        logger.debug(self.params)
-
-        # Postprocess (denormalise) and save fitted data
-        fit = self._postprocess(self.ydata, fit_norm)
-        self.fit = fit
-
-        self.residuals = residuals
-
-        self.coeffs = coeffs
-
-        # Calculate host molefraction from complexes and add as first row
-        molefrac_host = np.ones(molefrac.shape[1])
-        molefrac_host -= molefrac.sum(axis=0)
-        self.molefrac = np.vstack((molefrac_host, molefrac))
-
-    @property
     def statistics(self):
         """
-        Return fit statistics
+        Return fit statistics after parameter optimisation
 
         Returns:
-            Standard deviation of calculated y
-            Standard deviation of calculated coefficients
             Asymptotic error for non-linear parameter estimate
+            # Standard deviation of calculated y
+            # Standard deviation of calculated coefficients
         """
-        fit, residuals, coeffs, molefrac = self.fit # Fit results
-        # TODO deal with multi-y fit 3rd axis here? residuals is 3D
-        residuals_sum = np.square(residuals) # Sum of squares of residuals
+        # Calculate deLevie uncertainty
+        d = 1e-6 # delta
+        params = self._params_raw
 
-        # PLACEHOLDER add multi-y fit handling here
-        # Calculate degrees of freedom 
-        # = number of experimental datapoints - number of fitted parameters - 
-        # number of calculated coefficients
-        n = len(self.data["y"][0]) - len(self.params) - fit[2].size
-        sd_y = np.sqrt(ss/n)
+        # 0. Calculate partial differentials for each parameter
+        diffs = []
+        for i, pi in enumerate(params):
+            # Shift the ith parameter's value by delta
+            pi_shift = pi*(1 + d)
+            params_shift = np.copy(params)
+            params_shift[i] = pi_shift
 
-        pass
+            # Calculate fit with modified parameter set
+            x   = self.xdata
+            y   = self._preprocess(self.ydata)
+            fit_shift_norm, _, _, _ = self.function.objective(params_shift, 
+                                                        x, 
+                                                        y, 
+                                                        detailed=True,
+                                                        force_molefrac=True)
+            fit_shift = self._postprocess(self.ydata, fit_shift_norm)
+            
+            # Calculate partial differential
+            # Flatten numerator into 1D array (TODO: is this correct?)
+            num   = (fit_shift - self.fit).flatten()
+            denom = pi_shift - pi
+            diffs.append(num/denom)
+
+        diffs = np.array(diffs)
+
+        # 1. Calculate PxP matrix M and invert
+        P = len(params)
+        M = np.zeros((P, P))
+        for i, j in product(range(P), range(P)):
+            M[i, j] = np.sum(diffs[i]*diffs[j])
+        M_inv = np.linalg.inv(M)
+        m_diag = np.diagonal(M_inv)
+
+        # 2. Calculate standard deviations sigma of P parameters pi
+        # Sum of squares of residuals
+        ssr = np.sum(np.square(self.residuals))
+        # Degrees of freedom:
+        # N datapoints - N fitted params - N calculated coefficients
+        d_free = len(self.ydata[0]) - len(params) - self.coeffs.size
+
+        # TODO why d_free - 1?
+        sigma = np.sqrt((m_diag*ssr)/d_free)
+
+        # 3. Calculate confidence intervals
+        # Calculate t-value at 95%
+        # Studnt, n=d_free, p<0.05, 2-tail
+        t = stats.t.ppf(1 - 0.025, d_free)
+
+        ci = np.array([params - t*sigma, params + t*sigma])
+        ci_percent = (100*t*sigma)/params
+
+        return ci_percent
