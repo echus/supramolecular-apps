31b4db1530d6bf7fdb4de4a24b12ea9e726720a5
Varvara Efremova <varvara@echus.co>
2016-02-15 03:28:20 +0000

    Added search fields for Fit objects in Django admin

--- bindfit/admin.py
+++ bindfit/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Fit, Data
-admin.site.register(Data)
-admin.site.register(Fit)
+
+class DataAdmin(admin.ModelAdmin):
+    pass
+
+class FitAdmin(admin.ModelAdmin):
+    search_fields = ["meta_email", "meta_author", "meta_name", "meta_host", "meta_guest"]
+
+admin.site.register(Data, DataAdmin)
+admin.site.register(Fit,  FitAdmin)
