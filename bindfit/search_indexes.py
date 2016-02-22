548b65fddcb7376a4496f728e6596a1e52d7120b
Varvara Efremova <varvara@echus.co>
2016-02-22 06:16:02 +0000

    Added filtering for false searchable meta flags

--- bindfit/search_indexes.py
+++ bindfit/search_indexes.py
@@ -3,11 +3,10 @@ from . import models
 
 class FitIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    #author = indexes.CharField(model_attr='user')
 
     def get_model(self):
         return models.Fit 
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
+        return self.get_model().objects.filter(meta_options_searchable=True)
