69953ddf5ecbe79ae220a8d548ead3cc7d8507a9
Varvara Efremova <varvara@echus.co>
2016-03-21 04:12:35 +0000

    Basic working Monte Carlo error calculation (unchecked)

--- bindfit/fitter.py
+++ bindfit/fitter.py
@@ -55,22 +55,30 @@ class Fitter():
 
         return f 
 
-    def run_scipy(self, params_init):
+    def run_scipy(self, params_init, save=True, xdata=None, ydata=None):
         """
         Arguments:
-            params: dict  Initial parameter guesses for fitter    
+            params_init: dict  Initial parameter guesses for fitter    
+            save:        bool  If True, process and save optimisation results
+                               If False, return raw optimised params
+            xdata:       array Modified input array 
+            ydata:       array Modified input array 
+                               (used with save=False for Monte Carlo error 
+                               calculation)
         """
         logger.debug("Fitter.fit: called. Input params:")
         logger.debug(params_init)
+
+        # Set input data
+        x = self.xdata if xdata is None else xdata
+        y = self._preprocess(self.ydata if ydata is None else ydata)
         
+        # Sort parameter dict into ordered array
         p = []
         for key, value in sorted(params_init.items()):
             p.append(value)
-        
-        # Run optimizer 
-        x = self.xdata
-        y = self._preprocess(self.ydata)
 
+        # Run optimizer 
         tic = time.clock()
         result = scipy.optimize.minimize(self.function.objective,
                                          p,
@@ -95,35 +103,47 @@ class Fitter():
                                                     y, 
                                                     force_molefrac=True)
 
+        # Postprocessing
+        # Populate fit results dict
+        results = {}
+
         # Save time taken to fit
-        self.time = toc - tic 
+        results["time"] = toc - tic 
 
         # Save raw optimised params arra
-        self._params_raw = result.x
+        results["_params_raw"] = result.x
 
         # Postprocess (denormalise) and save fitted data
         fit = self._postprocess(self.ydata, fit_norm)
-        self.fit = fit
+        results["fit"] = fit
 
-        self.residuals = residuals
+        results["residuals"] = residuals
 
-        self.coeffs = coeffs
+        results["coeffs"] = coeffs
 
         # Calculate final molefrac (including host, function-specific)
-        self.molefrac = self.function.molefrac_plot(molefrac)
+        results["molefrac"] = self.function.molefrac_plot(molefrac)
 
         # Calculate fit uncertainty statistics
-        ci = self.statistics()
+        ci = self.statistics(result.x, fit, coeffs, residuals)
 
         # Save final optimised parameters and errors as dictionary
-        self.params = { name: {"value": param, "stderr": stderr, "init": params_init[name]} 
-                        for (name, param, stderr) 
-                        in zip(sorted(params_init), result.x, ci) }
+        results["params"] = { name: {"value": param, "stderr": stderr, "init": params_init[name]} 
+                              for (name, param, stderr) 
+                              in zip(sorted(params_init), result.x, ci) }
 
-        logger.debug("Fitter.run: PARAMS DICT")
-        logger.debug(self.params)
+        if save:
+            # Save fit results dict to object instance
+            for key, value in results.items():
+                setattr(self, key, value)
 
-    def statistics(self):
+            logger.debug("Fitter.run: CALCULATING MONTE CARLO (TEST)")
+            self.calc_monte_carlo(5, [0.02, 0.01], 0.005)
+        else:
+            # Return restuls dict without saving
+            return results
+
+    def statistics(self, params, fit, coeffs, residuals):
         """
         Return fit statistics after parameter optimisation
 
@@ -134,7 +154,6 @@ class Fitter():
         """
         # Calculate deLevie uncertainty
         d = np.float64(1e-6) # delta
-        params = self._params_raw
          
         # 0. Calculate partial differentials for each parameter
         diffs = []
@@ -151,12 +170,12 @@ class Fitter():
                                                         x, 
                                                         y, 
                                                         force_molefrac=True,
-                                                        fit_coeffs=self.coeffs)
+                                                        fit_coeffs=coeffs)
             fit_shift = self._postprocess(self.ydata, fit_shift_norm)
             
             # Calculate partial differential
             # Flatten numerator into 1D array (TODO: is this correct?)
-            num   = (fit_shift - self.fit).flatten()
+            num   = (fit_shift - fit).flatten()
             denom = pi_shift - pi
             diffs.append(np.divide(num, denom))
 
@@ -173,10 +192,10 @@ class Fitter():
 
         # 2. Calculate standard deviations sigma of P parameters pi
         # Sum of squares of residuals
-        ssr = np.sum(np.square(self.residuals))
+        ssr = np.sum(np.square(residuals))
         # Degrees of freedom:
         # N datapoints - N fitted params - N calculated coefficients
-        d_free = self.ydata.size - len(params) - self.coeffs.size
+        d_free = self.ydata.size - len(params) - coeffs.size
 
         sigma = np.sqrt((m_diag*ssr)/(d_free - 1))
 
@@ -189,3 +208,78 @@ class Fitter():
         ci_percent = (t*sigma)/params * 100
 
         return ci_percent
+
+    def calc_monte_carlo(self, n_iter, xdata_error, ydata_error):
+        """
+        Calculate error on fit using a Monte Carlo method
+
+        Arguments:
+            n_iter:      Number of Monte Carlo iterations
+            xdata_error: n array of n percentage errors corresponding to n 
+                         rows of xdata
+            ydata_error: Float corresponding to n percentage error on each
+                         row of ydata
+
+        Returns:
+            something
+        """
+
+        xdata = self.xdata
+        ydata = self.ydata
+        
+        # Convert params results to params_init array to use as input
+        # to run_scipy
+        # TODO: make params_init same format as params result
+        params_init = {}
+        for key, param in self.params.items():
+            params_init[key] = param["value"]
+
+        params_arr = np.zeros((n_iter, len(params_init)))
+
+        for n in range(n_iter):
+            # Calculate error multiplier arrays matching ydata, xdata shapes
+            xdata_error_arr = np.random.standard_normal(xdata.shape)\
+                              *ml.repmat(xdata_error, xdata.shape[1], 1).T\
+                              + 1
+            ydata_error_arr = np.random.standard_normal(ydata.shape)\
+                              *ydata_error\
+                              + 1
+
+            # Calculated shifted input data
+            xdata_shift = xdata*xdata_error_arr
+            ydata_shift = ydata*ydata_error_arr
+
+            logger.debug("Fitter.monte_carlo: params_init")
+            logger.debug(params_init)
+
+            results = self.run_scipy(params_init=params_init,
+                                     save       =False, 
+                                     xdata      =xdata_shift, 
+                                     ydata      =ydata_shift)
+
+            logger.debug("Fitter.monte_carlo: results")
+            logger.debug(results)
+
+            # Log resulting params
+            params_arr[n] = results["_params_raw"]
+
+        percentile_params = np.percentile(params_arr, [2.5, 97.5], axis=0).T
+
+        logger.debug("Fitter.monte_carlo: params_arr, percentile_params")
+        logger.debug(params_arr)
+        logger.debug(percentile_params)
+
+        # Calculate errors and update input params dict with results
+        for i, (key, param) in enumerate(sorted(self.params.items())):
+            p = param["value"]            # Actual param result
+            per = percentile_params[i] # Calc'd percentile for this param
+
+            lower = (100*(per[0] - p))/p
+            upper = (100*(per[1] - p))/p
+
+            param["mc"] = [lower, upper]
+
+        logger.debug("Fitter.monte_carlo: updated params dict")
+        logger.debug(self.params)
+
+        return self.params
